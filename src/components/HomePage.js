f4bb7d8b285ea844f7d4621908407af14de7dc57
Owusu Ansah Kwadwo <54241472+hacks-and-codes@users.noreply.github.com>
2023-03-04 02:04:42 +0000

    adding components

---
+++ src/components/HomePage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = ({ products, onAddToCart }) => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to Our E-commerce Store!</h1>
+      <p>Check out our Latest Products:</p>
+      <div className="product-list">
+        {products.map((product) => (
+          <div className="product" key={product.id}>
+            <img src={product.image} alt={product.name} />
+            <div className="product-details">
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+              <p className="product-price">${product.price.toFixed(2)}</p>
+              <button onClick={() => onAddToCart(product)}>Add to Cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
