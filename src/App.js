24fd61a1b22288afa5f0048d2b6afe11575ef23d
Owusu Ansah Kwadwo <54241472+hacks-and-codes@users.noreply.github.com>
2023-03-04 02:11:40 +0000

    adding css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import AdvancedSearch from "./AdvancedSearch";
-import ShoppingCart from "./ShoppingCart";
+import "./App.css";
+import "./components/HomePage.css";
+import AdvancedSearch from "./components/Search";
+import ShoppingCart from "./components/ShoppingCart";
+import Footer from "./components/Footer";
+import HomePage from "./components/HomePage";
 
 const App = () => {
   return (
     <div>
+      <HomePage />
       <AdvancedSearch />
       <ShoppingCart />
+      <Footer />
     </div>
   );
 };
