d15fb678921693510132c4fdd2fc0f29d5e21031
Owusu Ansah Kwadwo <54241472+hacks-and-codes@users.noreply.github.com>
2023-03-04 11:00:50 +0000

    adding different components

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item"><a href="#home" className="nav-link">Home</a></li>
+          <li className="nav-item"><a href="#shop" className="nav-link">Shop</a></li>
+          <li className="nav-item"><a href="#contact" className="nav-link">Contact</a></li>
+          <li className="nav-item"><a href="#cart" className="nav-link">Cart</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
